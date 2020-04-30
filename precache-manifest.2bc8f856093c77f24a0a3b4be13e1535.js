8a5a79c57ae9efe82a87bcb81062d4bfba653445
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-04-30 13:12:29 -0400

    Updates

---
+++ precache-manifest.2bc8f856093c77f24a0a3b4be13e1535.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8180cefde7c3f671864a0ed91431195",
+    "url": "/ct-videos/index.html"
+  },
+  {
+    "revision": "8ac1281f002559d808a8",
+    "url": "/ct-videos/static/css/main.0e1d659b.chunk.css"
+  },
+  {
+    "revision": "bf01afe4233a31ce3448",
+    "url": "/ct-videos/static/js/2.f2b4ec8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ct-videos/static/js/2.f2b4ec8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ac1281f002559d808a8",
+    "url": "/ct-videos/static/js/main.468df06c.chunk.js"
+  },
+  {
+    "revision": "f1ecb7fed90cd68cbd24",
+    "url": "/ct-videos/static/js/runtime-main.8312ec3d.js"
+  },
+  {
+    "revision": "d186c485a3ef967035eef56a604f0906",
+    "url": "/ct-videos/static/media/intro-to-ct_thumbnail.d186c485.jpg"
+  },
+  {
+    "revision": "c8deaf04981c15a4a30eeb65aad7d299",
+    "url": "/ct-videos/static/media/logo.c8deaf04.svg"
+  },
+  {
+    "revision": "5f4040bfe0b2ceba35378e9158e88077",
+    "url": "/ct-videos/static/media/plant-it-forward_thumbnail.5f4040bf.png"
+  },
+  {
+    "revision": "0eee1cf027fa9e3d02317ec76805f1d1",
+    "url": "/ct-videos/static/media/protection-amendment_thumbnail.0eee1cf0.png"
+  },
+  {
+    "revision": "d530c8bce5aa222e9b99f2e5bee694cd",
+    "url": "/ct-videos/static/media/theater-background-2.d530c8bc.jpg"
+  },
+  {
+    "revision": "a317ccf5d72f6d8443a64a7071fe63ed",
+    "url": "/ct-videos/static/media/ycyt-2_thumbnail.a317ccf5.jpg"
+  }
+]);
